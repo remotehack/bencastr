6f1e9218d53c8ec1dd0ff46c75f48fe0ee76ed53
Ben Foxall <benfoxall@gmail.com>
2021-02-28 19:54:06 +0000

    install tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 
+import "tailwindcss/tailwind.css"
 
 render(<App />, document.getElementById('root'))
 
