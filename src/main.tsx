2e674af7cae500c8e83fb7ab092978c64a88caae
Ben Foxall <benfoxall@gmail.com>
2021-03-01 21:57:58 +0000

    revert tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 
-import "tailwindcss/tailwind.css"
+import "./main.css"
 
 render(<App />, document.getElementById('root'))
 
