3e0efe7252afd6a385891cdf35cc61ab604fd5bb
Ben Foxall <benfoxall@gmail.com>
2021-03-07 23:51:56 +0000

    Switch to snowpack
    parcel wasn't playing well with coodespace ports

---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/' },
+    src: { url: '/_dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-webpack'
+  ],
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {
+    baseUrl: './',
+  },
+};
