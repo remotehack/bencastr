526541e5fd344cbf06ff9f3ea5ede6a893b617d9
Ben Foxall <benfoxall@gmail.com>
2021-03-09 06:59:42 +0000

    import styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 
+import 'chota';
 import "./main.css"
 
 render(<App />, document.getElementById('root'))
